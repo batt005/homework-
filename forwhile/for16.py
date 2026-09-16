24afda62e8eb70c820cd24cc88ed344c98703bfe
batt <batt125800@gmail.com>
2026-09-16 17:34:31 +0300

    Add files via upload

--- forwhile/for16.py
+++ forwhile/for16.py
@@ -1,8 +1,4 @@
 A = float(input('Введите А: '))
 N = int(input('Введите целое N (> 0): '))
-power = 1
-powers_list = []
-for i in range(1, N + 1):
-    power *= A
-    powers_list.append(power)
-print(powers_list)
+power = [A ** i for i in range(1, N+1)]
+print(power)
